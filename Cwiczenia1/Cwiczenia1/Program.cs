b5f6b4743f690b496f6b071ce4dcb7688f103d07
s17186 <s17186@pjwstk.edu.pl>
2020-03-01 18:04:35 +0100

    Z cwiczen

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,13 +1,32 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cwiczenia1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            //BLABLA
-            Console.WriteLine("Hello World!");
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://pja.edu.pl");
+            //ThreadPool()
+            //JAVA Future
+            if (result.IsSuccessStatusCode) //2xx
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z0-9]+@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                MatchCollection matches = regex.Matches(html);
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m);
+                }
+            }
+
+
+            Console.WriteLine("Bye World!");
         }
     }
 }
